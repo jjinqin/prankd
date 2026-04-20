58f4158bc1b36826f3a81e16d47cd9e1a8656fea
Aileen <ajane26@binb.idserve.net>
2026-04-20 12:22:37 +0800

    Initial Commit

---
+++ prank call/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  prank call
+//
+//  Created by Aileen Jane on 20/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ prank call/prank_callApp.swift
@@ -0,0 +1,17 @@
+//
+//  prank_callApp.swift
+//  prank call
+//
+//  Created by Aileen Jane on 20/04/26.
+//
+
+import SwiftUI
+
+@main
+struct prank_callApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
